4e8a6e564239ed81867650c79e0e9506b43bef51
Joao <joaogabrielsb36@gmail.com>
2024-03-06 21:45:52 -0300

    Feat: Add Class ScreenGame

---
+++ src/game/GameClass.java
@@ -0,0 +1,9 @@
+package game;
+
+public class GameClass {
+    public static void main(String[]args){
+
+        ScreenGame screenGame = new ScreenGame();
+        screenGame.setVisible(true);
+    }
+}
---
+++ src/game/ScreenGame.java
@@ -0,0 +1,11 @@
+package game;
+
+import javax.swing.*;
+
+public class ScreenGame extends JFrame {
+    public ScreenGame(){
+        setTitle("Snake Game");
+        setSize(800,800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
