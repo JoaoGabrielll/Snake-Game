25dcbc8624ffcbb2a147711d446e381f1e357b0f
Joao <joaogabrielsb36@gmail.com>
2024-03-15 18:45:01 -0300

    Inserting snake movement and depth methods

--- src/entities/Snake.java
+++ src/entities/Snake.java
@@ -1,4 +1,77 @@
 package entities;
 
+import entities.enums.Direction;
+
+import java.awt.*;
+import java.util.LinkedList;
+
 public class Snake {
+    private LinkedList<Point> body;
+    private Direction direction;
+
+    public Snake() {
+        body = new LinkedList<>();
+        body.add(new Point(5, 5));
+        direction = Direction.RIGHT;
+    }
+
+    public LinkedList<Point> getBody() {
+        return body;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void move(){
+        Point head = body.getFirst();
+        Point newHead = new Point(head);
+
+        switch (direction){
+            case UP:
+                newHead.y--;
+                break;
+            case DOWN:
+                newHead.y++;
+                break;
+            case RIGHT:
+                newHead.x--;
+                break;
+            case LEFT:
+                newHead.x++;
+                break;
+        }
+        body.addFirst(newHead);
+        body.removeLast();
+    }
+    public void grow(){
+        Point head = body.getFirst();
+        Point newHead = new Point(head);
+
+        switch (direction){
+            case UP:
+                newHead.y--;
+                break;
+            case DOWN:
+                newHead.y++;
+                break;
+            case RIGHT:
+                newHead.x--;
+                break;
+            case LEFT:
+                newHead.x++;
+                break;
+        }
+        body.addFirst(newHead);
+    }
+
+    public boolean checkCollision(Point point){
+        return body.contains(point);
+    }
+
 }
+
